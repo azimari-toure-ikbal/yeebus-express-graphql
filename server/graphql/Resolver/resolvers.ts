982b31c60b34a59a015906e0c7856d7ea462b1ca
azimari.toure.ikbal@gmail.com <azimari.toure.etu@esmt.sn>
2023-07-24 12:18:59 +0000

    Tests with Google Places API

--- server/graphql/Resolver/resolvers.ts
+++ server/graphql/Resolver/resolvers.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
+import {
+  Client,
+  Language,
+  PlaceInputType,
+} from "@googlemaps/google-maps-services-js";
 
+const client = new Client({});
 const baseUrl = "http://localhost:8080/api";
 
 export const resolvers = {
@@ -10,6 +16,12 @@ export const resolvers = {
       if (status !== 200) {
         throw new Error("Error fetching companies");
       }
+
+      let found_place_id = await findPlace();
+
+      let place_details = await getPlaceDetails(found_place_id);
+
+      console.log(place_details);
 
       return data;
     },
@@ -60,3 +72,55 @@ export const resolvers = {
     },
   },
 };
+
+async function findPlace(): Promise<string> {
+  try {
+    const response = await client.findPlaceFromText({
+      params: {
+        key: process.env.GOOGLE_MAPS_API_KEY,
+        input: "Mermoz Sacre-coeur, Dakar",
+        inputtype: PlaceInputType.textQuery,
+        language: Language.fr,
+      },
+    });
+
+    // Récupérer le place_id à partir de la réponse et l'affecter à found_place_id
+    return response.data.candidates[0].place_id;
+    // Vous pouvez également ajouter un console.log pour vérifier la valeur trouvée
+    console.log("Place ID trouvé :", response.data.candidates[0].place_id);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getPlaceDetails(place_id: string) {
+  try {
+    const response = await client.placeDetails({
+      params: {
+        key: process.env.GOOGLE_MAPS_API_KEY,
+        language: Language.en,
+        place_id: place_id,
+      },
+    });
+
+    return response.data.result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// async function getPlacePhoto(photo_reference: string) {
+//   try {
+//     const data = await client.placePhoto({
+//       params: {
+//         key: process.env.GOOGLE_MAPS_API_KEY,
+//         photoreference: photo_reference,
+//       },
+//       responseType: "stream",
+//     });
+
+//     return data.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
